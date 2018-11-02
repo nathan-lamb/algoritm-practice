cb9f11ccd50e5915c2c4bb9d0cf4b0d6f8820112
Nathan Lamb <nathanlamb022@gmail.com>
2018-11-02 19:10:26 +0100

    add reverse single word test

--- merging-meeting-times.test.js
+++ merging-meeting-times.test.js
@@ -1,16 +1,22 @@
 const { mergeRanges } = require('./merging-meeting-times')
+const { reverseString, reverseWords } = require('./reverse-string-inplace')
 
-it('mergeRanges is defined', () => {
-    expect(mergeRanges).toBeDefined()
+describe('mergeRanges tests', () => {
+    it('mergeRanges is defined', () => {
+        expect(mergeRanges).toBeDefined()
+    })
+    
+    it('should merge times where times.length > 2 && elements are in order', () => {
+        const times = [{ startTime: 1, endTime: 3 }, { startTime: 2, endTime: 4 }, { startTime: 4, endTime: 9 }]
+    
+        expect(mergeRanges(times)).toEqual(expect.arrayContaining([{startTime: 1, endTime: 9}]))
+    })
 })
 
-it('should merge times where times.length > 2 && elements are in order', () => {
-    const times = [
-        { startTime: 1, endTime: 3 },
-        { startTime: 2, endTime: 4 },
-        { startTime: 4, endTime: 9 }
-    ]
-
-    expect(mergeRanges(times)).toEqual(expect.arrayContaining([{startTime: 1, endTime: 9}]))
+describe('reverse strings and words', () => {
+    it('reverse a single word', () => {
+        const word = "hello"
+        expect(reverseString(word)).toEqual('olleh')
+    })
 })
 
