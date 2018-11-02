dca01baa0e866794373d4ebf48eb9c32bbb67d18
Nathan Lamb <nathanlamb022@gmail.com>
2018-11-02 19:59:55 +0100

    add tests

--- merging-meeting-times.test.js
+++ merging-meeting-times.test.js
@@ -8,15 +8,31 @@ describe('mergeRanges tests', () => {
     
     it('should merge times where times.length > 2 && elements are in order', () => {
         const times = [{ startTime: 1, endTime: 3 }, { startTime: 2, endTime: 4 }, { startTime: 4, endTime: 9 }]
-    
         expect(mergeRanges(times)).toEqual(expect.arrayContaining([{startTime: 1, endTime: 9}]))
     })
 })
 
 describe('reverse strings and words', () => {
     it('reverse a single word', () => {
-        const word = "hello"
-        expect(reverseString(word)).toEqual('olleh')
+        const string = 'hello'
+        expect(reverseString(string)).toEqual('olleh')
+    })
+
+    it('reverse a sentence', () => {
+        const string = 'hello I am a test string'
+        expect(reverseString(string)).toEqual('gnirts tset a ma I olleh')
+    })
+
+    it('reverseWords returns an array', () => {
+        const sentenceArray = []
+        expect(reverseWords(sentenceArray)).toEqual(expect.any(Array))
+    })
+    
+    it('reverse individual words keeping original word order', () => {
+        const sentenceArray = [ 't', 'h', 'e', ' ', 'e', 'a', 'g', 'l', 'e', ' ',
+        'h', 'a', 's', ' ', 'l', 'a', 'n', 'd', 'e', 'd' ]
+        expect(reverseWords(sentenceArray)).toEqual(expect.arrayContaining([ 'd', 'e', 'd', 'n', 'a', 'l', ' ', 's', 'a', 'h', ' ',
+        'e', 'l', 'g', 'a', 'e', ' ', 'e', 'h', 't' ]))
     })
 })
 
